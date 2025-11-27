5cfa5ccd7068985d6d26ea5eb8a848f19d2adf78
Blancardo135 <blanchardromain02@gmail.com>
2025-11-27 09:34:38 +0100

    Mise à jour du style

--- InfraDon2-projet/src/App.vue
+++ InfraDon2-projet/src/App.vue
@@ -5,11 +5,11 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
 
-    <div class="wrapper">
+    <!-- <div class="wrapper">
       <HelloWorld msg="Hey Boomer !" />
-    </div>
+    </div> -->
   </header>
 
   <main>
